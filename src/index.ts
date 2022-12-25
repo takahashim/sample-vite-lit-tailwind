b2d3d445f7bda39d3579ec9533aae213af4617d1
takahashim <takahashimm@gmail.com>
2022-12-25 22:37:16 +0900

    Use library mode

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { SampleElement } from './sample-element'
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
+import path from 'node:path';
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/my-element.ts',
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'SampleViteLitTailwind',
+      filename: 'sample-vite-lit-tailwind',
       formats: ['es'],
     },
     rollupOptions: {
       external: /^lit/,
+      output: {
+        globals: {
+          lit: 'Lit'
+        }
+      }
     },
+    sourcemap: true
   },
 })
